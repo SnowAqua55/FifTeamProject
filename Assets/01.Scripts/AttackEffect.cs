bc7de44c85edab5b4555a3a5a893a8297bb21f2b
t2024-m0215 <t2024-m0215@t2024-m0215ui-MacBookAir.local>
2025-06-13 19:46:31 +0900

    Revert "del : Player"
    This reverts commit 690da998edef86e05025317f6966895a16d3cf5a.

---
+++ Assets/01.Scripts/AttackEffect.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AttackEffect : MonoBehaviour
+{
+    [Tooltip("이펙트가 유지되는 시간")]
+    public float lifetime = 0.5f;
+    [Tooltip("이펙트가 줄 수 있는 데미지")]
+    public int damage = 1000;
+
+    void Start()
+    {
+        // 지정된 시간 후에 자동 파괴
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Monster")) // 맞춰서 수정해야하는 부분
+        {
+            // col.GetComponent<스크립트명>()?.TakeDamage(damage); > 구현에 맞춰서 수정
+            BossBase boss = col.GetComponentInParent<BossBase>();
+            if (boss != null && !boss.IsDead)
+            {
+                boss.TakeDamage(damage);
+            }
+        }
+    }
+}
