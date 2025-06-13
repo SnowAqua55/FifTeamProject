4a82eeb232b1d3597f3772bf05931e2da497ec4b
jungwoo57 <ghkdwjddn12@gmail.com>
2025-06-13 21:01:23 +0900

    feat : running보스 스크립트 추가

---
+++ Assets/01.Scripts/JWSTAGE/Stage1_Boss/RunningBoss.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningBoss : BossBase
+{
+    public override void InitStateMachine()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AttackPlayer()
+    {
+       //도망만 다니는 보스구현
+    }
+}
