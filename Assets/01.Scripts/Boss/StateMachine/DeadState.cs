e19c11af945405fcc4d20de47cfcbc593a280ff3
t2024-m0215 <t2024-m0215@t2024-m0215ui-MacBookAir.local>
2025-06-13 20:04:29 +0900

    복원용
    복원용

---
+++ Assets/01.Scripts/Boss/StateMachine/DeadState.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class DeadState : IBossState
+{
+    private BossBase boss;
+
+    public void Enter(BossBase boss)
+    {
+        this.boss = boss;
+        // 사망 애니메이션, 이펙트 실행
+        boss.Animator.SetTrigger(boss.AnimationData.DeadHash);
+
+        if (boss.rb != null)
+        {
+            boss.rb.velocity = Vector2.zero;
+            boss.rb.simulated = false; // 물리 계산 중지 (옵션)
+        }
+
+        Object.Destroy(boss.gameObject, 2f); // 2초 후 삭제
+    }
+
+    public void Update()
+    {
+        // 죽었으니 아무것도 안 함
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+    public void Exit()
+    {
+        // 
+    }
+}
