842a8737cd3b19618b3fd17af9e165764a69e580
jungwoo57 <ghkdwjddn12@gmail.com>
2025-06-16 10:41:14 +0900

    etc : 중간저장

--- Assets/01.Scripts/JWSTAGE/Stage1_Boss/RunningBoss.cs
+++ Assets/01.Scripts/JWSTAGE/Stage1_Boss/RunningBoss.cs
@@ -6,11 +6,16 @@ public class RunningBoss : BossBase
 {
     public override void InitStateMachine()
     {
-        throw new System.NotImplementedException();
+        ChangeState(new IdleState());
     }
 
     public override void AttackPlayer()
     {
        //도망만 다니는 보스구현
+       /*if (AttackRange >= Vector2.Distance(player.transform.position, transform.position))
+       {
+           //도망만가기
+       }
+       */
     }
 }
