85c3d606add0c063a52fae6fff851649289e4406
jungwoo57 <ghkdwjddn12@gmail.com>
2025-06-17 16:20:40 +0900

    fix : 보스 광폭화 패턴 수정 중

--- Assets/01.Scripts/JWSTAGE/Stage1_Boss/RunningBoss.cs
+++ Assets/01.Scripts/JWSTAGE/Stage1_Boss/RunningBoss.cs
@@ -13,7 +13,7 @@ public class RunningBoss : BossBase
     [Header("광폭화 패턴")]
     [SerializeField] private float rageTime;
     [SerializeField] private bool isRage;
-    [SerializeField] private GameObject rageAttack;
+    [SerializeField] private GameObject[] rageAttack;
     [SerializeField] private float stageStartTime;
     [SerializeField] private float attackApplyTime;
     protected override void Start()
@@ -30,6 +30,7 @@ public class RunningBoss : BossBase
 
     protected override void Update()
     {
+        base.Update();
         stageStartTime += Time.deltaTime;
         if (stageStartTime >= rageTime && isRage != true)
         {
@@ -54,7 +55,7 @@ public class RunningBoss : BossBase
         int ran = Random.Range(0, teleportPosition.Length);
         Vector2 telPosition =
             new Vector2(teleportPosition[ran].position.x + randomRange, teleportPosition[ran].position.y);
-        if(currentState.GetType() == new DeadState().GetType()) yield break; //deadstate상태일 때는 안하고싶은데 방법이 없을까 흠...
+        if(currentState.GetType() == new DeadState().GetType()) yield break; 
         Animator.SetTrigger("IsTeleport");
         yield return new WaitForSeconds(1.0f);
         if(currentState.GetType() == new DeadState().GetType()) yield break; 
@@ -62,7 +63,6 @@ public class RunningBoss : BossBase
         teleportCount++;
         if (teleportCount == 3) Attack();
         telCoroutine = null;
-        
     }
 
     private void Attack()
@@ -85,17 +85,19 @@ public class RunningBoss : BossBase
 
     IEnumerator RageAttack()
     {
-        int ran = Random.Range(0, rageAttack.transform.childCount);
-        GameObject attack = rageAttack.transform.GetChild(ran).gameObject;
+        Debug.Log("여긴잘됨");
+        int ran = Random.Range(0, rageAttack.Length);
+        GameObject attack = Instantiate(rageAttack[ran]);
+        attack.gameObject.SetActive(true);
         Collider2D[] col = attack.GetComponentsInChildren<Collider2D>();
         SpriteRenderer[] renderers = attack.GetComponentsInChildren<SpriteRenderer>();
         float timer = 0;
+        Color firstColor = new Color(248f / 255f, 92f / 255f, 92f / 255f, 60f / 255f);
+        Color lastColor = new Color(248f / 255f, 92f / 255f, 92f / 255f, 150f / 255f);
         while (timer < attackApplyTime)
         {
             timer += Time.deltaTime;
             float t = timer / attackApplyTime;
-            Color firstColor = new Color(248f / 255f, 92f / 255f, 92f / 255f, 60f / 255f);
-            Color lastColor = new Color(248f / 255f, 92f / 255f, 92f / 255f, 150f / 255f);
             for (int i = 0; i < renderers.Length; i++)
             {
                 renderers[i].color = Color.Lerp(firstColor, lastColor, t);   
@@ -110,7 +112,7 @@ public class RunningBoss : BossBase
         for (int i = 0; i < col.Length; i++)
         {
             col[i].enabled = false;
-            //renderers[i].color = firste
+            renderers[i].color = firstColor;
         }
     }
 }
